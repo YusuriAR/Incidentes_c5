66b857538050d5cb339f4c0d7160fe9d11905773
Yedam <66651590+yefovar@users.noreply.github.com>
2020-11-23 00:39:27 -0600

    Update utils.py
    Cambio para ingestion

--- src/utils/utils.py
+++ src/utils/utils.py
@@ -2,10 +2,10 @@ import pickle
 import os
 
 def save_df(df,path):
-    direction = os.path.join(path, "fe_df" + "." + "pkl")
-    pickle.dump(df,open(direction,"wb"))
+    #direction = os.path.join(path, "fe_df" + "." + "pkl")
+    pickle.dump(df,open(path,"wb"))
     
 def load_df(path):
-    direction = os.path.join(path, "fe_df" + "." + "pkl")
-    df = pickle.load(open(direction,"rb"))
+    #direction = os.path.join(path, "fe_df" + "." + "pkl")
+    df = pickle.load(open(path,"rb"))
     return df
