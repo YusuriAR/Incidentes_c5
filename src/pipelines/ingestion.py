1b80a3f83b233d46a3807d87be59e61afd307a8f
Yedam <66651590+yefovar@users.noreply.github.com>
2020-11-23 00:38:29 -0600

    Ingestion
    Revision completa de ejecucion

--- src/pipelines/ingestion.py
+++ src/pipelines/ingestion.py
@@ -1,33 +1,37 @@
 import pandas as pd
 import numpy as np
 import os
+from src.utils import utils
 
 def ingest_file(path):
     direction = os.path.join(path, "incidentes-viales-c5" + "." + "csv")
     df = pd.read_csv(direction)
     return df
 
-def drop_cols(df,columnas):
-    df.drop(columnas,axis='columns',inplace=True)
-    return df
+def drop_cols(df):
+    columns = ['geopoint','hora_cierre','fecha_cierre','año_cierre','mes_cierre','folio','delegacion_cierre']
+    drop_df = df.drop(columns, axis=1)
+    return drop_df
 
-# Crear variable de incidentes_reales
 def generate_label(df):
-    row_indexes = df[(df['codigo_cierre']=='(N) La unidad de atención a emergencias fue despachada, llegó al lugar de los hechos, pero en el sitio del evento nadie solicitó el apoyo de la unidad')].index    
-    row_aux = df[(df['codigo_cierre']=='(F) El operador/a o despachador/a identifican, antes de dar respuesta a la emergencia, que ésta es falsa. O al ser despachada una unidad de atención a emergencias en el lugar de los hechos se percatan que el incidente no corresponde al reportado inicialmente')].index
-    row_indexes.union(row_aux)
-    df.loc[row_indexes,'incidente_falso']="1"
-    df['incidente_falso'] = df['incidente_falso'].replace(np.nan, '0')
+    df['label'] = np.where((df['codigo_cierre'].str[:3] == '(F)')|(df['codigo_cierre'].str[:3] == '(N)'), 1, 0)
     return df
 
-def generate_label_color(df):
-    row_indexes = df[(df['codigo_cierre']=='(N) La unidad de atención a emergencias fue despachada, llegó al lugar de los hechos, pero en el sitio del evento nadie solicitó el apoyo de la unidad')].index    
-    row_aux = df[(df['codigo_cierre']=='(F) El operador/a o despachador/a identifican, antes de dar respuesta a la emergencia, que ésta es falsa. O al ser despachada una unidad de atención a emergencias en el lugar de los hechos se percatan que el incidente no corresponde al reportado inicialmente')].index
-    row_indexes.union(row_aux)
-    df.loc[row_indexes,'incidente_falso_color']="red"
-    df['incidente_falso_color'] = df['incidente_falso_color'].replace(np.nan, 'blue')
+def generate_label_incidente(df):
+    df['incidente_falso'] = np.where((df['codigo_cierre'].str[:3] == '(F)')|(df['codigo_cierre'].str[:3] == '(N)'), '1', '0')
     return df
 
-def save_ingestion(df, path):
-    return
+def generate_label_incidente_color(df):
+    df['incidente_falso_color'] = np.where((df['codigo_cierre'].str[:3] == '(F)')|(df['codigo_cierre'].str[:3] == '(N)'), 'red', 'blue')
+    return df
 
+def save_ingestion(df):
+    os.chdir('../')
+    path = 'output\ingest_df.pkl'
+    utils.save_df(df, path)
+
+def ingest(path):
+    df = ingest_file(path)
+    df = drop_cols(df)
+    df = generate_label(df)
+    save_ingestion(df)
